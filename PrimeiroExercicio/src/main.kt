a78857f4c22a6ec6ed405f203a23364aa4e6f50d
Vinicius Holanda <59942319+Holandavini@users.noreply.github.com>
2021-02-14 03:49:12 -0300

    Update main.kt
    Adicionando a constante para eliminar os magic numbers

--- PrimeiroExercicio/src/main.kt
+++ PrimeiroExercicio/src/main.kt
@@ -1,10 +1,12 @@
+const val ZERO: Double = 0.0
+const val CONTROLE_OPERACAO: Int = 4
+
 fun main() {
-    const val ZERO: Double = 0.0
     println("Olá, por favor escolha a operação que iremos realizar:")
     println("(1) Soma | (2) Subtração | (3) Multiplicação | (4) Divisão")
     val operacao: String? = readLine()!!
     if (operacao != null) {
-        if (operacao.toInt() <= 4) {
+        if (operacao.toInt() <= CONTROLE_OPERACAO) {
             println("Qual o primeiro número?")
             val a = readLine()!!
             val aFloat = a.toDouble()
@@ -16,7 +18,7 @@ fun main() {
                 "1" -> println("O resultado é: ${aFloat + bFloat}")
                 "2" -> println("O resultado é: ${aFloat - bFloat}")
                 "3" -> println("O resultado é: ${aFloat * bFloat}")
-                "4" -> if (bFloat != 0.0) {
+                "4" -> if (bFloat != ZERO) {
                     println("O resultado é: ${aFloat / bFloat}")
                 } else {
                     println("Não é possível dividir por 0! Tente novamente.")
